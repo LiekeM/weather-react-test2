e187e8f5030690d64c6613b5023098bf206b991e
LiekeM <liekevandermade@gmail.com>
2023-02-27 19:00:37 +0200

    FEATURE | added 5 day weather forcast in row/column

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,14 +1,26 @@
 import React from "react";
+import FormattedDate from "./FormattedDate";
+import TemperatureConversion from "./TemperatureConversion";
 import WeatherIcon from "./WeatherIcon";
-import "./WeatherForecast.css";
 
-export default function WeatherForecast() {
+export default function WeatherInfo(props) {
+  let name = props.data.city;
+  let temperature = Math.round(props.data.daily[props.day].temperature.day);
+  let minimumTemp = Math.round(props.data.daily[props.day].temperature.minimum);
+  let maximumTemp = Math.round(props.data.daily[props.day].temperature.maximum);
+  let icon = props.data.daily[props.day].condition.icon;
+  let date = new Date(props.data.daily[props.day].time * 1000);
+
   return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          {" "}
-          Thu <WeatherIcon code="04d" /> 19 10
+    <div className="WeatherInfo">
+      <div className="row mt-3">
+        <div className="col-6">
+          <FormattedDate date={date} />
+
+          <div>
+            {maximumTemp} °C | {minimumTemp} °C
+          </div>
+          <WeatherIcon code={icon} size={52} />
         </div>
       </div>
     </div>
